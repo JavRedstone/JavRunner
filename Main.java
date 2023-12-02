8c04e3711f808fa259cf9adebcba2b0076aab293
JavRedstone <javier.huang168@gmail.com>
2023-12-02 15:24:24 -0500

    update

--- Main.java
+++ Main.java
@@ -2,12 +2,12 @@ import opmodes.auto.sides.middle.*;
 
 public class Main {
     public static void main(String[] args) {
-        // LeftBlueAuto3 lba3 = new LeftBlueAuto3();
-        // lba3.runOpMode();
+        LeftBlueAuto3 lba3 = new LeftBlueAuto3();
+        lba3.runOpMode();
         // RightBlueAuto3 rba3 = new RightBlueAuto3();
         // rba3.runOpMode();
-        LeftRedAuto3 lra3 = new LeftRedAuto3();
-        lra3.runOpMode();
+        // LeftRedAuto3 lra3 = new LeftRedAuto3();
+        // lra3.runOpMode();
         // RightRedAuto3 rra3 = new RightRedAuto3();
         // rra3.runOpMode();
     }
